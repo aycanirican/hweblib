12a5e3a26f3a079c7ed62b774de1e7bdab422552
Aycan iRiCAN <iricanaycan@gmail.com>
2011-07-24 14:33:08 +0300

    Added more tests for rfc3986.

--- test/Test/Parser/Rfc3986.hs
+++ test/Test/Parser/Rfc3986.hs
@@ -1,8 +1,9 @@
-{-# LANGUAGE OverloadedStrings, PackageImports #-}
+{-# LANGUAGE OverloadedStrings #-}
 module Test.Parser.Rfc3986 where
 
 import Data.Attoparsec
 import qualified Data.ByteString.Char8 as C
+import Network.Parser.RfcCommon
 import Test.Parser.Parser
 import Test.HUnit
 import Network.Types
@@ -10,7 +11,7 @@ import Network.Parser.Rfc3986
 
 tests = TestList $ fmap TestCase lst
 
-lst = [test_uri]
+lst = [test_simple_query, test_simple_query, test_uri]
 test_uri 
     = let i = "http://user:pass@www.core.gen.tr/dir1/file.ext?param1=val1&param2=val2 "
           o = Just $ URI { uriScheme = "http"
@@ -21,3 +22,14 @@ test_uri
                          }
       in
         assertEqual "uri" o (aP uri i)
+
+test_simple_query =
+  let i = "foo=bar&zoo=yoo "
+      o = Just "?foo=bar&zoo=yoo"
+  in assertEqual "simple_query" o (fmap toRepr (aP query i))
+
+test_simple_query2 =
+  let i = "foo=bar;zoo=yoo "
+      o = Just "?foo=bar;zoo=yoo"
+  in assertEqual "simple_query2" o (fmap toRepr (aP query i))
+
