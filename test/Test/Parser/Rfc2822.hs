cb98d2b4d025e69ca29649c75e2822741014efef
Aycan iRiCAN <iricanaycan@gmail.com>
2014-02-08 12:05:03 +0000

    cosmetics.

--- test/Test/Parser/Rfc2822.hs
+++ test/Test/Parser/Rfc2822.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE OverloadedStrings #-}
+
 module Test.Parser.Rfc2822 where
 --------------------------------------------------------------------------------
 import Data.Attoparsec
@@ -6,11 +7,14 @@ import Data.ByteString as W
 import qualified Data.ByteString.Char8 as C
 import Test.Parser.Parser
 import Test.HUnit
+--------------------------------------------------------------------------------
 import Network.Parser.Rfc2822
 import Network.Parser.Rfc2234
 import Network.Parser.RfcCommon
 --------------------------------------------------------------------------------
+
 tests = TestList $ fmap TestCase lst
+
 lst = [test_angle_addr, test_name_addr]
 
 test_angle_addr = ae "angle_addr" (Just [117,115,101,114,49,64,101,120,46,111,114,103]) (aP angle_addr "<user1@ex.org>;")
