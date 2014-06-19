80e2f1deaeb7e4d02de6416d4e586372726fea8a
Aycan iRiCAN <iricanaycan@gmail.com>
2014-06-19 13:03:41 +0300

    simplified default.nix

--- default.nix
+++ default.nix
@@ -1,15 +1,13 @@
-{ nixpkgs ? import <nixpkgs> { }, hs ? nixpkgs.haskellPackages }:
-with nixpkgs;
+{ hs ? (import <nixpkgs> { }).haskellPackages }:
 
 let
     inherit (hs) cabal;
-in
-
-cabal.mkDerivation (self : rec {
+in cabal.mkDerivation (self : rec {
     pname = "hweblib";
     version = "0.6.3";
     isExecutable = false;
     isLibrary = true;
+    preConfigure = ''rm -rf dist'';
     src = ./.;
 
     buildDepends = [
